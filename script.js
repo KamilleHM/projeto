7b378667e2fa70367c1aac3d3826d5f07581baf8
kami <kamillehartmannm@gmail.com>
2024-01-08 08:42:39 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,43 @@
+function toggleMode(){
+    const html = document.documentElement 
+    // document é a representação do documento (pagina) em formato de JS
+    // Sempre que colocar um . (ponto) estou acessando propriedades e funcionalidades
+    // Entao dentro do documento . eu acesso o meu html atraves do documentElement
+
+
+    // if(html.classList.contains('light')){
+    //     html.classList.remove('light')
+    // } else{
+    //     html.classList.add('light')
+    // }
+    html.classList.toggle('light') //O toggle faz sozinho a condição acima (se tiver o ligth ele tira e se nao tiver o light ele coloca)
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img") //query é pesquisa selector pelo seletor
+    //substituir a img
+    if(html.classList.contains('light')){
+    //se tiver light mode, adc a img light
+    img.setAttribute('src', 'assets/avatar-light.png') //P ajustar/modificar um atributo (primeiro o atributo que quer mudar, e dps o novo valor que eu quero)
+    } else{
+    //se tiver sem light mode, manter a img normal
+    img.setAttribute('src', 'assets/avatar.png')
+    }
+}
+
+
+/* No JS tudo é objeto
+Atributos são as prorpiedades de um objeto
+Metodos sao as funcionalidades de um objeto 
+
+criando um objeto
+const person = {
+    age:18,
+    drive: function() {}
+}
+
+usando um objeto
+person.drive()*/
+
+
+
+
